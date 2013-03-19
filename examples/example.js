6f329b6c2e66871b495c30cc09dc944de0fa5cc3
Russell Madsen <ruzz311@gmail.com>
2013-03-19 17:40:07 -0500

    Example files added.

---
+++ examples/example.js
@@ -0,0 +1,19 @@
+var Butchershop = require('../index.js');
+
+// the "local" sub-object can be omitted with the same result.
+var butcher = new Butchershop({
+    local: {
+        host        : 'localhost',
+        port        : 8000
+    },
+    proxy: {
+        protocol    : 'http',
+        host        : 'npmjs.org',
+        port        : 80
+    }
+});
+
+// chop any calls to '/stylus/*' to be routed to a local directory
+butcher.chop('/stylus/{path*}', './stylus-local')
+
+butcher.start()
